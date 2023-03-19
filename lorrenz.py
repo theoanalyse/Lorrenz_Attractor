b29e5f29186db781c363a1756a1c16a5ed30cf2d
Théo André <46953688+theoanalyse@users.noreply.github.com>
2023-03-19 16:35:22 +0100

    FINAL VERSION (do not touch anymore)

--- lorrenz.py
+++ lorrenz.py
@@ -16,14 +16,15 @@ def df(X, t):
     dz = x*y -beta*z # dz/dt
     return np.array([dx, dy, dz]) # on retourne le vecteur X + dt*dF
 
-
 ## MAILLAGE EN TEMPS
-t = np.linspace(0, 100, 10001)
+Tmax = 100
+dt = 0.01
+t = np.arange(0, Tmax, dt)
 
 ## CONDITION INITIALE
 X0 = np.array([10, 10, 10])
 
-## ON RÉSOUD LE SYSTÈME
+## ON RÉSOUD LE SYSTÈME PAR ODEINT
 sol = odeint(df, X0, t)
 x = sol[:, 0]
 y = sol[:, 1]
@@ -33,8 +34,17 @@ z = sol[:, 2]
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(projection="3d")
 
-## ON DESSINE LE PORTRAIT DE PHASE
-plt.plot(x, y, z, color="black", lw=0.5)
+## NOMMER LES AXES
+ax.set_xlabel("x")
+ax.set_xlabel("y")
+ax.set_xlabel("z")
+
+## TITRE DE LA FIGURE
+ax.set_title(rf"Simulating Lorrenz Attractor with $\sigma$ = {sigma}, $\beta$ = {np.round(beta, 2)}, $\rho$ = {rho}")
+
+## ON DESSINE LE PORTRAIT DE PHASE OBTENU AVEC ODEINT ET E.E.
+plt.plot(x, y, z, color="black", lw=0.2, label="ODEINT")
 
 ## ON AFFICHE TOUT :D
+ax.legend()
 plt.show()
